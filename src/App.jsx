a097f9843d54c6daa0171c4813239ead83a9e0d3
Rifat-Earth <rifathasan01915@gmail.com>
2026-03-03 21:26:09 +0600

    setup tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar></Navbar>
+     <h1 className='text-2xl'>this is work...</h1>
+    </>
+  )
+}
+
+export default App
